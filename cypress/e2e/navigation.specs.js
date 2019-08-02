0841d65d6d861e6ac5a044d91cb4b76a3bbc481e
Jonathan Davis <jonathan@obj.co>
2019-08-02 00:09:49 -0600

    Updates dependencies

--- cypress/e2e/navigation.specs.js
+++ cypress/e2e/navigation.specs.js
@@ -3,8 +3,7 @@ describe('Navigation', () => {
     cy.visit('/').getByText(/into game design/i, { timeout: 500 })
   })
 
-  // TODO: Need to fix SPA routing to index.html on direct routes
-  it.skip('should handle routes that do not exist', () => {
+  it('should handle routes that do not exist', () => {
     cy.visit('/404').getByText(/page not found/i, { timeout: 500 })
   })
 })
